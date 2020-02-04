543746cff5b162a09eef66f0f3f1ed0e604638b4
Nils Hermansson <hermansson.nils@gmail.com>
2020-02-04 22:16:56 +0100

    added menu.js and removed the burger constructors from js_script.js

---
+++ public/js/menu.js
@@ -0,0 +1,54 @@
+let food = [
+    {
+        "name": "Nice Size Burger",
+        "kcal": 900,
+        "lact": true,
+        "glut": false,
+        "src": "https://www.burgerdudes.se/wp-content/uploads/2018/09/shadyburger_basic_burger_stefan_med.jpg",
+        "special": "Good Value!"
+
+    },
+
+    {
+        "name": "The Long Boy",
+        "kcal": 1200,
+        "lact": true,
+        "glut": true,
+        "src": "https://i.ytimg.com/vi/L6yX6Oxy_J8/maxresdefault.jpg",
+        "special": "Double patties!"
+
+    },
+
+    {
+        "name": "Veggie Delight xxl",
+        "kcal": 800,
+        "lact": false,
+        "glut": false,
+        "src": "https://veganheaven.org/wp-content/uploads/2018/06/Cauliflower-Burger-Vegan-Super-Crispy-11-500x500.jpg",
+        "special": "Good Value!"
+
+    },
+
+    {
+        "name": "The Big Boy",
+        "kcal": 1300,
+        "lact": true,
+        "glut": true,
+        "src": "https://i.pinimg.com/originals/80/40/9d/80409d8c06d21e0c0416a40c2176def3.jpg",
+        "special": "It's huge!"
+
+    },
+
+    {
+        "name": "The Chicken Nugget Burger",
+        "kcal": 850,
+        "lact": false,
+        "glut": true,
+        "src": "https://i.pinimg.com/originals/ba/ff/d9/baffd95b284f21e019a7a07ffb9d6d7e.jpg",
+        "special": "Contains chicken"
+
+    }
+
+
+
+]
