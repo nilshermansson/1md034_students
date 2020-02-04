a4b397871be550985fba33fbf1f6e1cbeb670e4e
Nils Hermansson <hermansson.nils@gmail.com>
2020-02-04 21:20:56 +0100

    remade menu using vue and pure js

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,3 +1,4 @@
+/*
 function menuItem (name, kcal, glut, lact, special, src){
     this.name = name;
     this.kcal = kcal;
@@ -19,50 +20,68 @@ let veggie = new menuItem("Veggie Delight xxl", 800, false, false, "Vegan", "htt
 let bigboy = new menuItem("The Big Boy", 1300, true, true, "It's big!", "https://i.pinimg.com/originals/80/40/9d/80409d8c06d21e0c0416a40c2176def3.jpg");
 
 let chicken = new menuItem("The Chicken Nugget Burger", 850, true, false, "Contains Chicken", "https://i.pinimg.com/originals/ba/ff/d9/baffd95b284f21e019a7a07ffb9d6d7e.jpg");
+*/
+/*
+function burgerName(burger){
+    let heading = document.createElement('h3');
+    let txt = document.createTextNode(burger.name);
+    heading.appendChild(txt);
+    return heading;
+}
 
-let list = document.createElement('ul');
+function burgerImg(burger){
+    let img = document.createElement('img');
+    img.width = "300";
+    img.height = "300";
+    img.src = burger.source;
+    return img;
+}
 
-let li1 = document.createElement('li');
-let li2 = document.createElement('li');
-let li3 = document.createElement('li');
-let li4 = document.createElement('li');
-let li5 = document.createElement('li');
+function burgerProperties(burger){
+    let list = document.createElement('ul');
 
-let txt1 = document.createTextNode(nicesize.info());
-let txt2 = document.createTextNode(longboy.info());
-let txt3 = document.createTextNode(veggie.info());
-let txt4 = document.createTextNode(bigboy.info());
-let txt5 = document.createTextNode(chicken.info());
+    let li1 = document.createElement('li');
+    let li2 = document.createElement('li');
 
-li1.appendChild(txt1);
-li2.appendChild(txt2);
-li3.appendChild(txt3);
-li4.appendChild(txt4);
-li5.appendChild(txt5);
+    let kcalTxt = document.createTextNode("kCal: " + burger.kcal);
+    let specialTxt = document.createTextNode(burger.special);
 
-list.appendChild(li1);
-list.appendChild(li2);
-list.appendChild(li3);
-list.appendChild(li4);
-list.appendChild(li5);
+    li1.appendChild(kcalTxt);
+    li2.appendChild(specialTxt);
 
-document.getElementById("myID").appendChild(list);
+    list.appendChild(li1);
+    list.appendChild(li2);
+
+    if(burger.lact){
+        let li4 = document.createElement('li');
+        let lactTxt = document.createTextNode("Contains Lactose");
+        li4.appendChild(lactTxt);
+        list.appendChild(li4);
+    }
+
+    if(burger.glut){
+        let li5 = document.createElement('li');
+        let glutTxt = document.createTextNode("Contains Gluten");
+        li5.appendChild(glutTxt);
+        list.appendChild(li5);
+    }
+    return list;
+}
+
+function makeBurger(burger){
+    let div =  document.createElement('div');
+    div.appendChild(burgerName(burger));
+    div.appendChild(burgerImg(burger));
+    div.appendChild(burgerProperties(burger));
+    div.class="box";
+    return div;
+}
 
 var burgers = [nicesize, longboy, veggie, bigboy, chicken];
 
-var list2 = document.createElement("ul");
-
-for (var burger in burgers){
-	  var li = document.createElement('li');
-    var txt = document.createTextNode(x.name);
-    li.appendChild(txt);
-    list.appendChild(li);
+for (i in burgers){
+    let currentBurger = burgers[i];
+    document.getElementById("menu").appendChild(makeBurger(currentBurger));
 }
 
-
-document.getElementById("myID").appendChild(list);
-
-
-
-
-
+*/
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,14 +1,32 @@
+function menuItem (name, kcal, glut, lact, special, src){
+    this.name = name;
+    this.kcal = kcal;
+    this.glut = glut;
+    this.lact = lact;
+    this.special = special;
+    this.source = src;
+    this.info = function() {
+        return this.name + ' kCal: ' + this.kcal;
+    }
+}
+
+let nicesize = new menuItem("Nice Size Burger", 900, true, false, "Good value!", "https://www.burgerdudes.se/wp-content/uploads/2018/09/shadyburger_basic_burger_stefan_med.jpg");
+
+let longboy = new menuItem("The Long Boy", 1200, true, true, "Double patties!", "https://i.ytimg.com/vi/L6yX6Oxy_J8/maxresdefault.jpg");
+
+let veggie = new menuItem("Veggie Delight xxl", 800, false, false, "Vegan", "https://veganheaven.org/wp-content/uploads/2018/06/Cauliflower-Burger-Vegan-Super-Crispy-11-500x500.jpg");
+
+let bigboy = new menuItem("The Big Boy", 1300, true, true, "It's big!", "https://i.pinimg.com/originals/80/40/9d/80409d8c06d21e0c0416a40c2176def3.jpg");
+
+let chicken = new menuItem("The Chicken Nugget Burger", 850, true, false, "Contains Chicken", "https://i.pinimg.com/originals/ba/ff/d9/baffd95b284f21e019a7a07ffb9d6d7e.jpg");
+
+
 const vm = new Vue({
-    el: '#myID',
+    el: '#menu',
     data: {
-        arbitraryVariableName: 'Välj en burgare!',
   	    burgers: [nicesize, longboy, veggie, bigboy, chicken]
     }
 })
-
-
-
-
 
 /*
   HTML:
@@ -18,10 +36,10 @@ const vm = new Vue({
   </h3>
   <ul>
   <li v-for="burger in burgers">
-  {{ burger.info() }} 
+  {{ burger.info() }}
   <b v-if="burger.glut">contains gluten</b>
   <b v-if="burger.lact">contains lactose</b>
-  </li>  
+  </li>
   </ul>
   </div>
 */
